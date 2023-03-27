7de36ab30cff5d0922f8a71eee4367c5ff9d75c0
Joshua Ferreras <joshuaferreras790@gmail.com>
2023-03-27 10:16:30 +0000

    Staging

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import LoginForm from '@/components/LoginForm.vue';
+
 </script>
 
 <template>
-<LoginForm></LoginForm>
+
 </template>
---
+++ src/views/UserRoleView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <img src="images/role-doctor.jpg" alt="Avatar">
+      <div class="role-name">
+        <h4><b>Doctor</b></h4>
+      </div>
+    </div>
+    <div class="card">
+      <img src="images/role-patient.jpg" alt="Avatar">
+      <div class="role-name">
+        <h4><b>Patient</b></h4>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+.container{
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background: #11499C;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img{
+  position: relative;
+  width: 100%;
+  height: 50%;
+
+}
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 100px 40px 80px 0 rgba(0,0,0,0.9);
+  transition: 0.3s;
+  margin:  40px;
+  background-color: #11499C;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 20px 20px 0 rgba(0,0,0,0.9);
+  transform: scale(1.1);
+}
+
+/* Add some padding inside the card container */
+.role-name {
+  padding: 2px 16px;
+}
+
+.role-name h4{
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+}
+</style>
