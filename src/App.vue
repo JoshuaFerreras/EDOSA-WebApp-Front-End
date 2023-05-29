ce35162cd3dc19d629a792f0d6dfe23347ed36a1
Gabby <gabrieloprin@gmail.com>
2023-05-29 08:11:28 +0000

    Gab 20230529

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const user = ref(null);
+onMounted(async () => {
+  const data = await axios.get('/me');
+  user.value = data.data.data;
+});
 </script>
 
 
 <template>
   <div id="app">
-    <RouterView />
+    <RouterView :user="user" />
   </div>
 </template>
 
 <style>
-*{
+* {
   margin: 0;
 }
-  #app {
-  
-    font-family: "Poppins", sans-serif;
-    font-size: 1.6rem;
-    color: #222;
-    background: white;
-    height: 100vh; 
-    /* display: flex; */
-    justify-content: center;
-    align-items: center;
-  }
+
+#app {
+
+  font-family: "Poppins", sans-serif;
+  font-size: 1.6rem;
+  color: #222;
+  background: white;
+  height: 100vh;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/axios.js
+++ src/axios.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 axios.defaults.headers.put['Accept'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
